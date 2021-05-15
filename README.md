61c71c40edd0565ea455dea083dec154941ff002
Grégoire Hébert <gregoire@les-tilleuls.coop>
2021-05-15 14:21:36 +0200

    feat (tp): P1C5-correction

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # 1665806-Programmez-en-oriente-objet-PHP
 
-## TP P1C5
+## TP P1C5 correction de l'exercice
 
-Nous avons vu comment créer une classe, définir la visibilité des propriétés et des méthodes, et définir des constantes, ainsi que la staticité, et maintenant que vous avez les méthodes magiques dans votre arsenal ! Il est temps de vous exercer ! Reprenons notre cas fil rouge, Matchmaker, je vais vous donner un extrait de code.
+Voici un exemple de résultat dans le fichier `index.php`
+Vous aviez la même chose ?
+Super !
 
-Votre mission est d’utiliser un constructeur, un accesseur et un mutateur dans la classe `Player` :)  
-N'oubliez pas de mettre à jour le reste de votre code ;)
+Vous êtes prêt pour la suite :D
--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class Encounter
 
     public static function probabilityAgainst(Player $playerOne, Player $playerTwo): float
     {
-        return 1/(1+(10 ** (($playerTwo->level - $playerOne->level)/400)));
+        return 1/(1+(10 ** (($playerTwo->getLevel() - $playerOne->getLevel())/400)));
     }
 
     public static function setNewLevel(Player $playerOne, Player $playerTwo, int $playerOneResult): void
@@ -18,20 +18,35 @@ class Encounter
             trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', self::RESULT_POSSIBILITIES)));
         }
 
-        $playerOne->level += round(32 * ($playerOneResult - self::probabilityAgainst($playerOne, $playerTwo)));
+        $playerOne->setLevel(
+            $playerOne->getLevel() +
+            round(32 * ($playerOneResult - self::probabilityAgainst($playerOne, $playerTwo)))
+        );
     }
 }
 
 class Player
 {
-    public int $level;
+    private int $level;
+
+    public function __construct(int $level)
+    {
+        $this->level = $level;
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): void
+    {
+        $this->level = $level;
+    }
 }
 
-$greg = new Player;
-$jade = new Player;
-
-$greg->level = 400;
-$jade->level = 800;
+$greg = new Player(400);
+$jade = new Player(800);
 
 echo sprintf(
         'Greg à %.2f%% chance de gagner face a Jade',
@@ -44,8 +59,8 @@ Encounter::setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg->level,
-    $jade->level
+    $greg->getLevel(),
+    $jade->getLevel()
 );
 
 exit(0);
