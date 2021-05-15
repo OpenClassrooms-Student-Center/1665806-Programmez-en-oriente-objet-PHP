a7942aee2ce53912c3a530d0f1e7311267186bf8
Grégoire Hébert <gregoire@les-tilleuls.coop>
2021-05-15 14:19:29 +0200

    feat (tp): P1C3

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # 1665806-Programmez-en-oriente-objet-PHP
 
-## TP P1C3
+## TP P1C3 correction de l'exercice
 
-Maintenant que nous avons vu comment créer une classe, définir la visibilité des propriétés et des méthodes, et définir des constantes, il est temps de vous exercer ! Vous vous souvenez de notre cas fil rouge, Matchmaker ? Voici ce qu’on va faire. Je vais commencer par vous donner un squelette de code simple, écrit de manière fonctionnelle qui pourrait être le système de classement. Au fur et à mesure des exercices de ce cours, nous transformerons notre code avec les notions apprises au cours du chapitre. Pour arriver à un code que vous pourriez exploiter tel quel, ou dans n’importe quel framework web :)
+Vous y êtes arrivé ! Félicitations :)
+Vous trouverez un exemple d'implémentation dans le fichier index.php
 
-Votre mission, si vous l’acceptez, est de :
+## Suite de l'exercice
 
-- Créer les classes `Encounter`
-- Remplacer les fonctions fournies dans le code par des méthodes de classe
-- Et si des valeurs peuvent être considérées comme immuables, alors les transformer en constantes :)
+Rendez-vous sur la branche p1c3-suite avant de passer à la suite du cours !
--- index.php
+++ index.php
@@ -1,35 +1,42 @@
 <?php
 
-const RESULT_WINNER = 1;
-const RESULT_LOSER = -1;
-const RESULT_DRAW = 0;
-const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
+declare(strict_types=1);
 
-function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo)
+class Encounter
 {
-    return 1/(1+(10 ** (($againstLevelPlayerTwo - $levelPlayerOne)/400)));
-}
+    public const RESULT_WINNER = 1;
+    public const RESULT_LOSER = -1;
+    public const RESULT_DRAW = 0;
+    public const RESULT_POSSIBILITIES = [self::RESULT_WINNER, self::RESULT_LOSER, self::RESULT_DRAW];
 
-function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
-{
-    if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
-        trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+    public function probabilityAgainst(int $levelPlayerOne, int $againstLevelPlayerTwo): float
+    {
+        return 1 / (1 + (10 ** (($againstLevelPlayerTwo - $levelPlayerOne) / 400)));
     }
 
-    $levelPlayerOne += round(32 * ($playerOneResult - probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+    public function setNewLevel(int &$levelPlayerOne, int $againstLevelPlayerTwo, int $playerOneResult)
+    {
+        if (!in_array($playerOneResult, self::RESULT_POSSIBILITIES)) {
+            trigger_error(sprintf('Invalid result. Expected %s', implode(' or ', self::RESULT_POSSIBILITIES)));
+        }
+
+        $levelPlayerOne += (int) (32 * ($playerOneResult - $this->probabilityAgainst($levelPlayerOne, $againstLevelPlayerTwo)));
+    }
 }
 
 $greg = 400;
 $jade = 800;
 
+$encounter = new Encounter;
+
 echo sprintf(
-        'Greg à %.2f%% chance de gagner face a Jade',
-        probabilityAgainst($greg, $jade)*100
-    ).PHP_EOL;
+    'Greg à %.2f%% chance de gagner face a Jade',
+    $encounter->probabilityAgainst($greg, $jade)*100
+).PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
-setNewLevel($greg, $jade, RESULT_WINNER);
-setNewLevel($jade, $greg, RESULT_LOSER);
+$encounter->setNewLevel($greg, $jade, Encounter::RESULT_WINNER);
+$encounter->setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
