e2d8f39670a6657dfd2a44ae17cee097c9363bc6
Grégoire Hébert <gregoire@les-tilleuls.coop>
2021-05-15 15:17:08 +0200

    feat (tp): P2C4-correction

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Reprenons la solution du précédent exercice.
 Modifiez le code pour garantir l'extensibilité des méthodes, créez une classe abstraite pour les joueurs et utilisez le mot clé final.
 
 Si vous voyez d'autres modifications utiles, effectuez-les :)
+
+## Correction
+
+Le solution que j'ai choisi est : 
+
+```php
+abstract class AbstractPlayer {
+    public function __construct(public string $name = 'anonymous', public float $ratio = 400.0)
+    {
+    }
+
+    abstract protected function getName(): string;
+    abstract protected function getRatio(): float;
+    abstract protected function probabilityAgainst(AbstractPlayer $player): float;
+    abstract public function updateRatioAgainst(AbstractPlayer $player, int $result): void;
+}
+```
+
+La classe `Player` hérite désormais de la classe abstraite, et toutes les références à la classe `Player` font maintenant référence à la classe `AbstractPlayer`.
--- index.php
+++ index.php
@@ -41,23 +41,34 @@ class Lobby
     }
 }
 
-class Player
+abstract class AbstractPlayer
 {
-    public function __construct(protected string $name, protected float $ratio = 400.0)
+    public function __construct(public string $name = 'anonymous', public float $ratio = 400.0)
     {
     }
 
+    abstract public function getName(): string;
+
+    abstract public function getRatio(): float;
+
+    abstract protected function probabilityAgainst(self $player): float;
+
+    abstract public function updateRatioAgainst(self $player, int $result): void;
+}
+
+class Player extends AbstractPlayer
+{
     public function getName(): string
     {
         return $this->name;
     }
 
-    protected function probabilityAgainst(self $player): float
+    protected function probabilityAgainst(AbstractPlayer $player): float
     {
         return 1 / (1 + (10 ** (($player->getRatio() - $this->getRatio()) / 400)));
     }
 
-    public function updateRatioAgainst(self $player, int $result): void
+    public function updateRatioAgainst(AbstractPlayer $player, int $result): void
     {
         $this->ratio += 32 * ($result - $this->probabilityAgainst($player));
     }
@@ -70,7 +81,7 @@ class Player
 
 class QueuingPlayer extends Player
 {
-    public function __construct(Player $player, protected int $range = 1)
+    public function __construct(AbstractPlayer $player, protected int $range = 1)
     {
         parent::__construct($player->getName(), $player->getRatio());
     }
