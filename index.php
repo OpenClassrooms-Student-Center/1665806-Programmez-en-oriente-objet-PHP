6be5ffb379139f6d616e61192a3283f05d562487
yurim36 <45293454+yurim36@users.noreply.github.com>
2023-01-18 15:30:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 
-const RESULT_WINNER = 1;
-const RESULT_LOSER = -1;
-const RESULT_DRAW = 0;
-const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
 
 class Encounter
 {
+    private const RESULT_WINNER = 1;
+    private const RESULT_LOSER = -1;
+    private const RESULT_DRAW = 0;
+    private const RESULT_POSSIBILITIES = [RESULT_WINNER, RESULT_LOSER, RESULT_DRAW];
+    
+    
     public function probabilityAgainst(Player $playerOne, Player $playerTwo): float
     {
         return 1/(1+(10 ** (($playerTwo->level - $playerOne->level)/400)));
@@ -14,8 +16,8 @@ class Encounter
 
     public function setNewLevel(Player $playerOne, Player $playerTwo, int $playerOneResult): void
     {
-        if (!in_array($playerOneResult, RESULT_POSSIBILITIES)) {
-            trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', RESULT_POSSIBILITIES)));
+        if (!in_array($playerOneResult, self::RESULT_POSSIBILITIES)) {
+            trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', self::RESULT_POSSIBILITIES)));
         }
 
         $playerOne->level += round(32 * ($playerOneResult - $this->probabilityAgainst($playerOne, $playerTwo)));
@@ -41,8 +43,8 @@ echo sprintf(
     ).PHP_EOL;
 
 // Imaginons que greg l'emporte tout de même.
-$encounter->setNewLevel($greg, $jade, RESULT_WINNER);
-$encounter->setNewLevel($jade, $greg, RESULT_LOSER);
+$encounter->setNewLevel($greg, $jade, self::RESULT_WINNER);
+$encounter->setNewLevel($jade, $greg, self::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
