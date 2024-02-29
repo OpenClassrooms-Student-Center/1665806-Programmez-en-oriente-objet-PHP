b2b25f8a384da13f5187f89e9ec505c36b9f661f
Teo <teosany@icloud.com>
2024-02-29 09:13:52 +0100

    add declaration of dynamically declared variables, which is deprecated

--- src/MatchMaker/Encounter/Encounter.php
+++ src/MatchMaker/Encounter/Encounter.php
@@ -19,7 +19,8 @@ class Encounter
 {
     public const STATUS_PLAYING = 'En cours';
     public const STATUS_OVER = 'Terminé';
-
+    public int $scorePlayerA;
+    public int $scorePlayerB;
     protected int $dateOfEncounter;
 
     protected string $status = self::STATUS_PLAYING;
